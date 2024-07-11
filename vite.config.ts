d7ecf5f1cf41e8b043955376acf23d861b5028b4
Henrique Souza <henriquetsi10@gmail.com>
2024-07-11 12:57:43 -0300

    feat: add suport ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig, loadEnv, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode = '' }: UserConfig) => {
-	const env = loadEnv(mode, process.cwd());
+	const {
+		VITE_PORT,
+	} = loadEnv(mode, process.cwd());
 
 	return {
-		plugins: [react()],
+		plugins: [
+			react(),
+		],
 		server: {
-			port: Number(env.VITE_PORT),
+			port: Number(VITE_PORT),
+			https: {
+				key: fs.readFileSync('.cert/key.pem'),
+				cert: fs.readFileSync('.cert/cert.pem'),
+			},
 		},
 	}
 });
