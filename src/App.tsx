c0ee60849ede70ee7c3f5164d45a295080c02093
Henrique Souza <henriquetsi10@gmail.com>
2024-08-27 15:31:23 -0300

    feat: v0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { useRoutes } from 'react-router-dom';
 import routes from '~react-pages';
 
 const App = () => (
-	<Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
+	<Suspense fallback={<p>Loading...</p>}>
+		{useRoutes(routes)}
+	</Suspense>
 );
 
 export default App;
